5167d9c56474e164fd31d922ed83baee0b176d65
Tom Hadfield <tomhadfield66@googlemail.com>
2022-08-09 14:24:39 +0100

    clean up

---
+++ MoleculeGenerationApp/generate.py
@@ -0,0 +1,272 @@
+import sys
+sys.path.append("../")
+sys.path.append("./DeLinker/analysis/")
+sys.path.append("./DeLinker/")
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem.Draw import MolDrawing, DrawingOptions, MolToFile
+from rdkit.Chem import MolStandardize
+
+import numpy as np
+
+from itertools import product
+from joblib import Parallel, delayed
+import re
+from collections import defaultdict
+
+from IPython.display import clear_output
+IPythonConsole.ipython_useSVG = True
+
+from DeLinker_test import DenseGGNNChemModel
+from analysis import frag_utils, rdkit_conf_parallel
+from data.prepare_data import read_file, preprocess
+from examples import example_utils
+
+import glob
+import pandas as pd
+
+
+
+
+def run_DeLinker(webapp_session_dir, coords_to_replace, 
+                         data_path_fname = "./DeLinker_test_data",
+                         json_out_fname = "DeLinker_test", 
+                         num_elabs = 250,
+                         output_smi_id = 'gen_linkers', image_fname = 'linked_mols.svg'):
+    #Load in molecule(s)
+    mols = [Chem.MolFromMolFile(x) for x in glob.glob(f'{webapp_session_dir}/ligand*.sdf')]
+    #mol = Chem.MolFromMolFile(sdf_file)
+  
+    #Obtain substructure we're trying to replace via the atomic coordinates
+    linker, fragments, mol_smiles, mol_out = match_coords_to_indices(mols, coords_to_replace)
+    
+
+    
+    #Prepare the model to be run
+    model_args = process_pharmacophoric_info(mol_smiles, linker, fragments, mol_out,
+                                           data_path_fname=f'{webapp_session_dir}/DeLinker_test_data.smi',
+                                           json_out_fname=f'{webapp_session_dir}/molecules_DeLinker_test',
+                                           num_elabs=num_elabs,
+                                           output_smi_file=f'{webapp_session_dir}/DeLinker_{output_smi_id}.smi')
+  
+    #Make elabs
+    generate_linkers(model_args)
+
+    #Summary Statistics
+    mol_img = summary_stats(output_smi_file=f'{webapp_session_dir}/DeLinker_{output_smi_id}.smi', image_fname=f'{webapp_session_dir}/{image_fname}')
+
+    return mol_img
+
+
+def check_DeLinker_input(webapp_session_dir, coords_to_replace, 
+                             image_fname = 'DeLinker_input.svg'):
+    
+    
+    #print(webapp_session_dir, coords_to_replace)
+    #Load in molecule(s)
+    mols = [Chem.MolFromMolFile(x) for x in glob.glob(f'.{webapp_session_dir}/ligand*.sdf')]
+    #mol = Chem.MolFromMolFile(sdf_file)
+    
+    
+    #Obtain substructure we're trying to replace via the atomic coordinates
+    linker, fragments, mol_smiles, mol_out = match_coords_to_indices(mols, coords_to_replace)
+    
+    mol_out_2d = Chem.MolFromSmiles(fragments)
+    MolToFile(mol_out_2d, f'.{webapp_session_dir}/{image_fname}', size = (500, 500))
+
+    #Summary Statistics
+    #mol_img = summary_stats(output_smi_file=f'{webapp_session_dir}/DeLinker_{output_smi_id}.smi', image_fname=f'{webapp_session_dir}/{image_fname}')
+
+    return 0
+
+
+
+
+
+
+
+def generate_linkers(args):
+    model = DenseGGNNChemModel(args)
+    model.train()
+  
+    model = '' #Free up some memory
+
+    return 0
+
+
+def process_pharmacophoric_info(full_smi, linker_smi, fragments_smi, original_mol,
+                                data_path_fname,
+                                json_out_fname, 
+                                output_smi_file,
+                                num_elabs = 250):
+
+    
+    if linker_smi != "":
+        
+        len_linker = Chem.MolFromSmiles(linker_smi).GetNumHeavyAtoms()
+    
+    else:
+        
+        len_linker = 6 #TODO - change so user can select from drop down menu
+        
+        
+    dist, ang = frag_utils.compute_distance_and_angle(original_mol, linker_smi, fragments_smi)
+        
+    with open(data_path_fname, 'w') as f:
+        f.write("%s %s %s" % (fragments_smi, dist, ang))
+        
+    raw_data = read_file(data_path_fname)
+    preprocess(raw_data, "zinc", None, True, full_name = json_out_fname)    
+    
+    # Arguments for DeLinker
+    args = defaultdict(None)
+    args['--dataset'] = 'zinc'
+    args['--config'] = '{"generation": true, \
+                         "batch_size": 1, \
+                         "number_of_generation_per_valid": %d, \
+                         "min_atoms": %d, "max_atoms": %d, \
+                         "train_file": "%s.json", \
+                         "valid_file": "%s.json", \
+                         "output_name": "%s"}' % (num_elabs, len_linker, len_linker, 
+                                                              json_out_fname, json_out_fname, 
+                                                              output_smi_file)
+    args['--freeze-graph-model'] = False
+    args['--restore'] = './DeLinker/models/pretrained_DeLinker_model.pickle'
+    
+
+    return args
+
+def match_coords_to_indices(mols, coords, linking = True):
+  
+    if len(mols) == 1:
+        #i.e. only a single molecule provided and 
+        #the user has clicked a substructure to be removed
+        
+        mol = mols[0]
+        remove_indices = []
+        for c in coords:
+    
+            for atom in mol.GetAtoms():
+                if np.linalg.norm(np.array(mol.GetConformer().GetAtomPosition(atom.GetIdx())) - c) < 0.05:
+                    remove_indices.append(atom.GetIdx())
+    
+        print(f'Indices to be removed from Molecule: {remove_indices}')
+        elab_ev = []
+        frag_ev = []
+        for idx in remove_indices:
+            for nei in mol.GetAtomWithIdx(idx).GetNeighbors():
+                if nei.GetIdx() not in remove_indices:
+                    elab_ev.append(idx)
+                    frag_ev.append(nei.GetIdx())
+    
+        for idx in range(len(elab_ev)):
+            print(f'Exit vector pair {idx + 1}: {frag_ev[idx]}, {elab_ev[idx]}')
+                    
+    
+        bonds_to_break = [mol.GetBondBetweenAtoms(elab_ev[x],frag_ev[x]).GetIdx() for x in range(len(elab_ev))]
+        fragmented_mol = Chem.FragmentOnBonds(mol, bonds_to_break)
+        fragmentation = Chem.MolToSmiles(fragmented_mol).split('.')
+        
+        if linking:
+            fragments = []
+            for fragment in fragmentation:
+                if len([x for x in fragment if x =="*"]) ==2:
+                    linker=fragment
+                else:
+                    fragments.append(fragment)
+            fragments = '.'.join(fragments)
+            linker = re.sub('[0-9]+\*', '*', linker)
+            fragments = re.sub('[0-9]+\*', '*', fragments) 
+        
+            return linker, fragments, Chem.MolToSmiles(mol), mol
+        
+    elif len(mols) == 2:
+        #i.e. two fragments have been provided and we just want to link them
+        
+      
+        
+        if coords.shape[0] != 2:
+            raise ValueError('For linking two separate fragments you can only select two atoms!')
+         
+            
+        print(Chem.MolToSmiles(mols[0]))
+        print(Chem.MolToSmiles(mols[1]))
+        
+        combo_no_exit = Chem.CombineMols(mols[0],mols[1])
+        combo = Chem.CombineMols(combo_no_exit, Chem.MolFromSmiles("*.*"))
+        edcombo = Chem.EditableMol(combo)
+        num_heavy_atoms = combo.GetNumHeavyAtoms()
+        
+        add_dummy_atoms = []
+        
+        for atom in combo.GetAtoms():
+            for c in coords:
+                if np.linalg.norm(np.array(combo.GetConformer().GetAtomPosition(atom.GetIdx())) - c) < 0.05:
+                    add_dummy_atoms.append(atom.GetIdx())
+        
+        
+        edcombo.AddBond(num_heavy_atoms, add_dummy_atoms[0], order=Chem.rdchem.BondType.SINGLE)
+        edcombo.AddBond(num_heavy_atoms+1, add_dummy_atoms[1], order=Chem.rdchem.BondType.SINGLE)
+        editedcombo = edcombo.GetMol()
+        _ = AllChem.Compute2DCoords(editedcombo)
+        Chem.SanitizeMol(editedcombo)
+        
+        print(Chem.MolToSmiles(combo))
+        
+        mol_to_link = edcombo.GetMol()
+        Chem.SanitizeMol(mol_to_link)
+        
+        # Convert exit vectors to carbons for conformer generation
+        du = Chem.MolFromSmiles('*')
+        mol_to_link_carbon = AllChem.ReplaceSubstructs(mol_to_link,du,Chem.MolFromSmiles('C'),True)[0]
+        Chem.SanitizeMol(mol_to_link_carbon)
+        # Generate conformer
+        mol_to_link_carbon = Chem.AddHs(mol_to_link_carbon)
+        AllChem.ConstrainedEmbed(mol_to_link_carbon, combo_no_exit, randomseed=42)
+        mol_to_link_carbon = Chem.RemoveHs(mol_to_link_carbon)
+
+
+        # Add this conformer to the two unlinked fragments
+        conf = mol_to_link.GetConformer()
+        ref_conf = mol_to_link_carbon.GetConformer()
+        for i in range(mol_to_link_carbon.GetNumAtoms()):
+            pos = list(ref_conf.GetAtomPosition(i))
+            conf.SetAtomPosition(i, pos)
+        conf.SetId(0)
+        _ = mol_to_link.AddConformer(conf)
+
+
+
+        return "", Chem.MolToSmiles(mol_to_link), "", mol_to_link
+        
+        
+        
+
+
+def summary_stats(output_smi_file, image_fname = None):
+
+    DeLinker_mols = pd.read_csv(output_smi_file, sep = ' ')
+    DeLinker_mols.columns = ['frag', 'full', 'gen']
+
+    gen_vc = DeLinker_mols['gen'].value_counts()
+    #img = Chem.Draw.MolsToGridImage([Chem.MolFromSmiles(x) for x in list(gen_vc.index)[:12]], molsPerRow = 3, returnPNG=False)
+    
+    #img.save(image_fname)
+    mols_per_row=3
+
+    svg = Draw.MolsToGridImage([Chem.MolFromSmiles(x) for x in list(gen_vc.index)[:12]], molsPerRow=mols_per_row, useSVG=True)
+    #svg.save(image_fname)
+    with open(image_fname, 'w') as f:
+        f.write(svg.data)
+
+    #if filename is not None:
+    #    if not filename.endswith('.svg'):
+    #        filename += '.svg'
+    #    with open(filename, 'w') as f:
+    #        f.write(svg)
+    return svg 
+
+    #return img
---
+++ MoleculeGenerationApp/static/app.js
@@ -0,0 +1,280 @@
+let coordStore = []
+let filesToUpload = []
+let filesToUploadFull = []
+
+function uploadFile(form){ 
+	const formData = new FormData(form);
+	let oOutput = document.getElementById("static_file_response")
+	var oReq = new XMLHttpRequest();
+	oReq.open("POST", "upload_static_file", true);
+	oReq.onload = function(oEvent) {
+     		if (oReq.status == 200) {
+       			oOutput.innerHTML = "Successfully uploaded ligand file!";
+       			console.log(oReq.response)
+     		} else {
+       			oOutput.innerHTML = "Error occurred when trying to upload your file.<br \/>";
+     		}
+     	}; 
+
+	oOutput.innerHTML = "Sending file!";
+ 	console.log("Sending file!")
+	oReq.send(formData);
+
+}
+
+
+
+
+function uploadMultipleFiles(form){ 
+	const formData = new FormData(form);
+	let oOutput = document.getElementById("static_file_response")
+	var oReq = new XMLHttpRequest();
+	oReq.open("POST", "upload_multiple_static_files", true);
+	oReq.onload = function(oEvent) {
+     		if (oReq.status == 200) {
+       			oOutput.innerHTML = "Successfully uploaded files";
+       			console.log(oReq.response)
+       			//console.log(JSON.parse(oReq.response).files_to_upload)
+       			filesToUpload = filesToUpload.concat(JSON.parse(oReq.response).files_to_upload)
+       			console.log('Files to upload variable')
+       			console.log(filesToUpload)
+     		} else {
+       			oOutput.innerHTML = "Error occurred when trying to upload your file.<br \/>";
+     		}
+     	}; 
+
+	oOutput.innerHTML = "Sending file!";
+ 	console.log("Sending file!")
+	oReq.send(formData);
+
+}
+
+
+
+
+function displayMol(img_id, input_id){
+    console.log(img_id)
+    console.log(document.body.getElementsByTagName("*"))
+    console.log(document.getElementById(input_id).files[0].name)
+    console.log("/static/" + document.getElementById(input_id).files[0].name)
+    //document.getElementById(img_id).src = "/static/imgs/" + document.getElementById(input_id).files[0].name
+    document.getElementById(img_id).src = "/static/imgs/mol_with_indices.png"
+    
+    $("#select_index").removeClass('hidden')
+}
+
+
+function displayLinkedMols(webapp_session_directory, img_id){
+    //console.log(img_id)
+    //console.log(document.body.getElementsByTagName("*"))
+    //console.log(document.getElementById(input_id).files[0].name)
+    //console.log("/static/" + document.getElementById(input_id).files[0].name)
+    //document.getElementById(img_id).src = "/static/imgs/" + document.getElementById(input_id).files[0].name
+    //document.getElementById(img_id).src = "/static/imgs/linked_mols.svg"
+    document.getElementById(img_id).src = webapp_session_directory + '/linked_mols.svg'
+    
+    //$("#select_index").removeClass('hidden')
+}
+
+
+function displayDeLinkerInput(webapp_session_directory, img_id){
+    
+    document.getElementById("check_input_div").style.display="block"
+    document.getElementById(img_id).src = webapp_session_directory + '/DeLinker_input.svg'
+    
+ 
+}
+
+
+
+
+function display3DMol(){
+
+        coordStore.length = 0 //delete any existing coordinates which have been saved
+        
+        let element = $('#mol_3d');
+        let config = { backgroundColor: 'white' };
+        let viewer = $3Dmol.createViewer( element, config );
+        let pdbUri = '/static/imgs/ligand.sdf';
+        //let pdbUri="{{ upload_folder }} + '/ligand1.sdf'"
+        
+       
+        console.log(pdbUri)
+        jQuery.ajax( pdbUri, { 
+            success: function(data) {
+            
+              $("#instructions").hide();
+              let v = viewer;
+              v.addModel( data, "sdf" );                       /* load data */
+              v.setStyle({stick:{radius:0.15}});               /* style all atoms */
+              
+              v.setClickable({},true,function(atom,viewer,event,container) { 
+                        v.addSphere({center: {x:atom.x, y:atom.y, z:atom.z}, radius: 0.5, color:'red'})
+                        v.render()
+                        coordStore.push([atom.x, atom.y, atom.z])     
+                   });
+                   
+              v.zoomTo();                                      /* set camera */
+              v.render();                                      /* render scene */
+              v.zoom(1.2, 1000);                               /* slight zoom */
+            },
+            error: function(hdr, status, err) {
+              console.error( "Failed to load PDB " + pdbUri + ": " + err );
+            },
+        });
+    
+    }
+
+
+
+
+function display3DMol_2(molDir){
+
+        coordStore.length = 0 //delete any existing coordinates which have been saved
+        
+        let element = $('#mol_3d');
+        let config = { backgroundColor: 'white' };
+        let viewer = $3Dmol.createViewer( element, config );
+        //let pdbUri = '/static/imgs/ligand.sdf';
+        //let pdbUri="{{ upload_folder }} + '/ligand1.sdf'"
+        
+        
+        //Get full file path of molecules we want to view
+        for (let i = 0; i < filesToUpload.length; i++){
+            filesToUploadFull.push(molDir + "/" +filesToUpload[i])
+        }
+        
+        console.log(filesToUploadFull)
+        
+        
+        for (let i = 0; i < filesToUploadFull.length; i++){
+
+            let pdbUri = filesToUploadFull[i]        
+            console.log(pdbUri)
+            
+            if (i == 0){
+                //Get rid of instructions
+                $("#instructions").hide();
+            }
+            
+            
+            if (pdbUri.includes("ligand")){
+            
+            jQuery.ajax( pdbUri, { 
+            success: function(data) {
+                viewer.addModel( data, "sdf" );                       /* load data */
+                viewer.setStyle({stick:{radius:0.15}});               /* style all atoms */    
+                   
+                viewer.setClickable({},true,function(atom,viewer,event,container) { 
+                    console.log('Clicked')
+                    viewer.addSphere({center: {x:atom.x, y:atom.y, z:atom.z}, radius: 0.5, color:'red'})
+                    viewer.render()
+                    coordStore.push([atom.x, atom.y, atom.z])     
+                   });
+                   
+                viewer.zoomTo();                                      /* set camera */
+                viewer.render();                                      /* render scene */
+                viewer.zoom(1.2, 1000);                               /* slight zoom */
+                
+                
+                console.log('Here')
+                
+                
+                },
+            error: function(hdr, status, err) {
+              console.error( "Failed to load file " + pdbUri + ": " + err );
+                },
+            });
+            
+                
+            } else {
+                
+            jQuery.ajax( pdbUri, { 
+            success: function(data) {
+                viewer.addModel( data, "pdb" );                       /* load data */
+                viewer.zoomTo();                                      /* set camera */
+                viewer.render();                                      /* render scene */
+                viewer.zoom(1.2, 1000);                               /* slight zoom */
+                },
+            error: function(hdr, status, err) {
+              console.error( "Failed to load file " + pdbUri + ": " + err );
+                },
+            });
+            
+            }
+            
+            
+            
+
+            
+       
+        } //end for loop
+    } //end function 
+
+
+
+
+function printSavedCoords(){
+    console.log(coordStore)
+}
+
+
+$(document).ready(function () {
+    $("#save_coords").on("click", function() {
+        var js_data = JSON.stringify(coordStore);
+        $.ajax({                        
+            url: '/save_array',
+            type : 'post',
+            contentType: 'application/json',
+            dataType : 'json',
+            data : js_data
+        }).done(function(result) {
+            console.log(result);
+            /*$("#data").html(result);*/
+            $("#data").html('Successfully saved coordinates!')
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log("fail: ",textStatus, errorThrown);
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    $("#generate_elabs").on("click", function() {
+    
+        $("#text_generating").attr("style", "display:block")
+    
+    
+        var js_data = JSON.stringify(coordStore);
+        $.ajax({                        
+            url: '/generate_linkers',
+            type : 'post',
+            contentType: 'application/json',
+            dataType : 'json',
+            data : js_data
+        }).done(function(result) {
+           
+            /*console.log(result);*/
+            /*$("#data").html(result);*/
+            $("#data").html('Successfully generated linkers!')
+             $("#text_generating").attr("style", "display:none")
+             $("#finished_generating").attr("style", "display:block")
+            
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log("fail: ",textStatus, errorThrown);
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+  /*
+            
+            */
+
